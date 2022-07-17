b01b8fd871b3f20043e38e2edbd0d11be0af7207
Faith <fhgray96@gmail.com>
2022-07-16 21:09:29 -0500

    update template

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Card from './Card'
+import Hero from './Hero.jsx'
+import Navbar from './Navbar'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Hero />
+    <Card />
   </React.StrictMode>
 )
